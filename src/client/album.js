380128b80db09985065a07c681cdad0816f57358
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-11-13 21:27:22 +0100

    Use CollectionView for track listing in album page

--- src/client/album.js
+++ src/client/album.js
@@ -12,8 +12,16 @@ shared.openAlbumPage = function(album) {
     scaleMode: "fill"
   }).appendTo(page);
 
-  var titleLabel = tabris.create("Label", {
-    background: "#cecece"
+  var trackListView = tabris.create("CollectionView", {
+    itemHeight: 25,
+    initializeCell: function(cell) {
+      var label = tabris.create("Label", {
+        layoutData: {left: 10, right: 10, top: 1, bottom: 1}
+      }).appendTo(cell);
+      cell.on("itemchange", function(track) {
+        label.set("text", track.name || track.path);
+      });
+    }
   }).appendTo(page);
 
   var playButton = tabris.create("Button", {
@@ -29,13 +37,13 @@ shared.openAlbumPage = function(album) {
 
   function update() {
     page.set("title", index.name || "unknown album");
-    titleLabel.set("text", getTracks().join("\n"));
+    trackListView.set("items", getTracks());
   }
 
   function play() {
     var url = config.SERVER + "/replace";
     var data = getTracks().map(function(track) {
-      return config.SERVER + "/albums/" + album.path + "/" + encodeURIComponent(track);
+      return config.SERVER + "/albums/" + album.path + "/" + encodeURIComponent(track.path);
     });
     $.post(url, JSON.stringify(data));
   }
@@ -44,28 +52,27 @@ shared.openAlbumPage = function(album) {
     if ("discs" in index) {
       return Array.prototype.concat.apply([], index.discs.map(function(disc) {
         return disc.tracks.map(function(track) {
-          return disc.path + "/" + track.path;
+          track.path = disc.path + "/" + track.path;
+          return track;
         });
       }));
     }
-    return index.tracks.map(function(track) {
-      return track.path;
-    });
+    return index.tracks;
   }
 
   function layout() {
     var bounds = page.get("bounds");
-    var coverSize = Math.floor(Math.min(bounds.width, bounds.height) / 2);
+    var coverSize = Math.floor(bounds.width / 3);
     if (bounds.width > bounds.height) {
       // landscape
-      playButton.set("layoutData", {left: 10, top: 10});
-      coverView.set("layoutData", {left: 10, centerY: 0, width: coverSize, height: coverSize});
-      titleLabel.set("layoutData", {left: [coverView, 10], top: 10, right: 10, bottom: 10});
+      trackListView.set("layoutData", {left: 10, top: 10, bottom: 10, right: [30, 10]});
+      playButton.set("layoutData", {right: 10, top: 10});
+      coverView.set("layoutData", {right: 10, bottom: 10, width: coverSize, height: coverSize});
     } else {
       // portrait
+      coverView.set("layoutData", {left: 10, top: 10, width: coverSize, height: coverSize});
       playButton.set("layoutData", {right: 10, top: 10});
-      coverView.set("layoutData", {centerX: 0, top: 10, width: coverSize, height: coverSize});
-      titleLabel.set("layoutData", {left: 10, top: [coverView, 10], right: 10, bottom: 10});
+      trackListView.set("layoutData", {left: 10, top: [coverView, 10], right: 10, bottom: 10});
     }
   }
 
