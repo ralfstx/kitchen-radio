8495b854258231bcf31445751a1c954f2d69489a
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-01-05 17:28:57 +0100

    Improve logging config

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -7,14 +7,23 @@ var Config = require("./config");
 var logger = new Winston.Logger({
   transports: [
     new Winston.transports.Console({
+      level: "trace",
       handleExceptions: true,
-      level: "debug",
-      colorize: true
+      prettyPrint: true,
+      colorize: true,
+      silent: false,
+      timestamp: false
     }),
     new Winston.transports.File({
       filename: Path.join(Config.logDir, "debug.log"),
-      handleExceptions: true,
+      maxsize: 40000,
+      maxFiles: 10,
       level: "debug",
+      handleExceptions: true,
+      prettyPrint: false,
+      colorize: true,
+      silent: false,
+      timestamp: true,
       json: false
     })
   ],
