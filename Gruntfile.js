58dadeb42c01f47bd94b4c72cf4fce4aa6753b62
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-09-24 20:08:34 +0200

    Improve build, update dependencies
    Write build output to build/ folder.
    Include package.json in build to use npm install on the server.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,20 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
+    clean: ["build"],
     jshint: {
       options: {
         jshintrc: true
       },
       all: ["Gruntfile.js", "src/**/*.js", "test/**/*.js"]
+    },
+    copy: {
+      source: {
+        expand: true,
+        cwd: "src/",
+        src: ["**/*"],
+        dest: "build/"
+      }
     },
     rsync: {
       options: {
@@ -16,7 +25,7 @@ module.exports = function(grunt) {
       },
       dist: {
         options: {
-          src: "./src/",
+          src: "build/",
           dest: "/var/lib/node/",
           host: "pi@radio",
           delete: true // Careful this option could cause data loss, read the docs!
@@ -25,9 +34,18 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.registerTask("package", "create package.json", function() {
+    var pkg = grunt.file.readJSON("package.json");
+    delete pkg.devDependencies;
+    grunt.file.write("build/package.json", JSON.stringify(pkg));
+  });
+
   grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-rsync");
 
-  grunt.registerTask("default", ["jshint", "rsync"]);
+  grunt.registerTask("build", ["clean", "copy:source",  "package"]);
+  grunt.registerTask("default", ["jshint", "build", "rsync"]);
 
 };
